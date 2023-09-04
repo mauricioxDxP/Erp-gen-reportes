f2f674a997c0b4c4ea16122b50cbdef39f55fd05
mauricioxDxP <calderonxdp@hotmail.com>
2023-09-04 18:23:44 -0400

    subir

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,73 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
-
+import { Button } from '@mui/base'
+import CodeEditor from '@uiw/react-textarea-code-editor';
 function App() {
-  const [count, setCount] = useState(0)
+  const [valores,setValores]=useState({
+    clase:"",
+    generadoFrontend:""
+  });
+  const handleChange = (x:React.ChangeEvent<HTMLTextAreaElement|undefined>)=>
+  {
+    setValores({
+      ...valores,
+      [x.target.name]:x.target.value,
+    });
+   generar(x.target.value);
+  }
+  const generar = (entrada:string)=>{
+    setValores({
+      ...valores,
+      generadoFrontend:logicaGenerar(entrada),
+    })
+  };
+function logicaGenerar(entrada:string){
+  var c = entrada.split("\n");
+  let nombreComponente=c.shift();
+  let cols:string="";
+  let rows:string="   {";
+  c.forEach((campo)=>{
+    let [name,title]=campo.split(",");
+    cols += `   { name: '${name}', title: '${title}' },\n`;
+    rows +=` ${name}:""`;
+  });
 
+  var template = `
+  import React, { useState } from 'react'
+  import DeleteIcon from 'icons/DeleteIcon';
+  import Edit from 'icons/Edit';
+  import { DataTablaStandar } from 'components/data-table/data-table-standar';
+
+  export const ${nombreComponente} = () => {
+    const [rows, setRows] = useState([
+${rows}])
+    const [columns, setColumns] = useState([
+${cols}]);
+  }
+    `
+  return template;
+}
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <div style={{ display: 'flex', width: "100vw", gap: "10px" }}>
+        <CodeEditor style={{ width: '-webkit-fill-available', height: "90vh" }}
+          value={valores.clase}
+          onChange={handleChange}
+          name='clase'
+          language="csharp"
+          data-color-mode='dark'
+        ></CodeEditor>
+
+        <CodeEditor style={{ width: '-webkit-fill-available', height: "90vh" }}
+          value={valores.generadoFrontend}
+          name='generado'
+          language="jsx"
+          data-color-mode='dark'
+        ></CodeEditor>
+    </div>
+    {/* <div style={{display:'flex'}}>
+      <Button style={{margin:"auto"}} onClick={generar}>convertir</Button>
+    </div> */}
     </>
   )
 }
